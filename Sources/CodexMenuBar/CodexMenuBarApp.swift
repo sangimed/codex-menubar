e9f4304d0f1a9e446f4af7b3a37ef3f3223d7950
Mohamed I. <med.ibrihen@gmail.com>
2026-08-31 00:06:10 +0200

    feat: wire v0.2 application services

--- Sources/CodexMenuBar/CodexMenuBarApp.swift
+++ Sources/CodexMenuBar/CodexMenuBarApp.swift
@@ -7,23 +7,69 @@ struct CodexMenuBarApp: App {
     private var appDelegate
 
     @StateObject
-    private var store = UsageStore()
+    private var preferences: PreferencesStore
+
+    @StateObject
+    private var store: UsageStore
+
+    @StateObject
+    private var launchAtLogin =
+        LaunchAtLoginManager()
+
+    init() {
+        let preferences =
+            PreferencesStore()
+
+        _preferences =
+            StateObject(
+                wrappedValue:
+                    preferences
+            )
+
+        _store =
+            StateObject(
+                wrappedValue:
+                    UsageStore(
+                        preferences:
+                            preferences
+                    )
+            )
+    }
 
     var body: some Scene {
         MenuBarExtra {
-            MenuBarView(store: store)
+            MenuBarView(
+                store: store,
+                preferences:
+                    preferences,
+                launchAtLogin:
+                    launchAtLogin
+            )
         } label: {
-            MenuBarLabel(store: store)
-                .task {
-                    store.start()
-                }
+            MenuBarLabel(
+                store: store,
+                preferences:
+                    preferences
+            )
+            .task {
+                store.start()
+                launchAtLogin
+                    .refreshStatus()
+            }
         }
         .menuBarExtraStyle(.window)
     }
 }
 
-private final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
+private final class AppDelegate:
+    NSObject,
+    NSApplicationDelegate
+{
+    func applicationDidFinishLaunching(
+        _ notification: Notification
+    ) {
+        NSApp.setActivationPolicy(
+            .accessory
+        )
     }
 }
